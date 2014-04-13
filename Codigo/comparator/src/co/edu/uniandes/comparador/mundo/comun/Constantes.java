692443bf2669df377d64c980605666d046dc19d0
fabianbecerra <fabianbp@gmail.com>
2014-04-13 12:29:07 -0500

    Pruebas para Clases comunes

---
+++ Codigo/comparator/src/co/edu/uniandes/comparador/mundo/comun/Constantes.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.comparador.mundo.comun;
+
+/**
+ *
+ * @author FabianEduardo
+ */
+public class Constantes {
+
+    /**
+     * Ruta y nombre del archivo de propiedades 
+     */
+    public static String PROPERTIES_FILE_NAME = "comparador-config.properties";
+    
+    /**
+     * Cargar las propiedades
+     */
+    public static final LectorPropiedades propertiesLoader = LectorPropiedades.getInstance();
+    
+    /**
+     * Label de lineas agregadas
+     */
+    public static String LABEL_ADD_LINE = propertiesLoader.getProperty("comparator.addline");
+
+    
+}
